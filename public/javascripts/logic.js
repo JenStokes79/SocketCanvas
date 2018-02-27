3e0e5109afa9500561a5ede8d8b2fcb1056f315c
wildcat510 <bloomjazzer@gmail.com>
2018-02-27 12:24:29 -0800

    handles connect/disconnect on server and client side

--- public/javascripts/logic.js
+++ public/javascripts/logic.js
@@ -5,7 +5,7 @@ client.on('join', function(data) { //move to game.js
         $('#status_msg').text('Waiting for more people... Feel free to draw!')
     } else if (Object.keys(data).length >= 2) {
         $('#status_msg').text('Let the games begin!')
-            //initialize game
+            //io.emit initialize game
     }
     //update users list on DOM
     $('#user').html('');
@@ -17,6 +17,12 @@ client.on('join', function(data) { //move to game.js
 //Handle disconnect on client side
 client.on('disconnect', function(data) {
     console.log(data);
+    if (Object.keys(data).length < 2) {
+        $('#status_msg').text('Waiting for more people... Feel free to draw!')
+    } else if (Object.keys(data).length >= 2) {
+        $('#status_msg').text('Let the games begin!')
+            //io.emit initialize game
+    }
     $('#user').html('');
     for (key in data) {
         $('#user').append(`<div>${data[key].name}<br>wins: ${data[key].wins}<br><br></div>`)
